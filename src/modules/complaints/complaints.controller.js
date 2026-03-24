81dcce57beb1a4996557a9cc3a4f24494bd9fc2c
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2026-03-24 00:16:42 -0400

    feat(complaints): implementa module de exclusão de denúncia - adiciona controller de exclusão com tratamento de 404 - implementa exclusão de denúncia no Firestore - implementa regra de negócio para exclusão de denúncia

--- src/modules/complaints/complaints.controller.js
+++ src/modules/complaints/complaints.controller.js
@@ -1,6 +1,7 @@
 import * as complaintService from './complaints.service.js';
 import { success, error } from '../../shared/utils/responseUtils.js';
 
+/** @type {import("express").RequestHandler} */
 export const create = async (req, res) => {
   try {
     const complaint = await complaintService.create(req.body);
@@ -10,6 +11,7 @@ export const create = async (req, res) => {
   }
 };
 
+/** @type {import("express").RequestHandler} */
 export const getAll = async (req, res) => {
   try {
     const complaint = await complaintService.getAll();
@@ -30,3 +32,16 @@ export const getDetail = async (req, res) => {
     return error(res, err.message, 400);
   }
 }
+
+/** @type {import("express").RequestHandler} */
+export const deleteComplaint = async (req, res) => {
+  try {
+    const complaint = await complaintService.deleteComplaint(req.params.id);
+    return success(res, complaint, 200);
+  } catch (err) {
+    if (err.message === "Denúncia não encontrada"){
+      return error(res, err.message, 404);
+    }
+    return error(res, err.message, 400);
+  }
+};
