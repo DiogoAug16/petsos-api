daaef38d4a5cf6abadadc0230a75d4961d24ee11
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2026-03-25 10:41:53 -0400

    refactor: simplifica os controllers com HOF e remove os blocos de try-catch

--- src/routes/complaints.routes.js
+++ src/routes/complaints.routes.js
@@ -1,11 +1,14 @@
-import { Router } from 'express';
-import * as complaintController from '../modules/complaints/complaints.controller.js';
+import { Router } from "express";
+import * as complaintController from "../modules/complaints/complaints.controller.js";
+import { wrap } from "../shared/utils/controllerHOF.js";
+import { validateUploadImage } from "../validators/upload.validator.js";
+import { prepareComplaintData } from "../validators/complaint.validator.js";
 
 const router = Router();
 
-router.post('/', complaintController.create);
-router.get('/', complaintController.getAll);
-router.get('/:id', complaintController.getDetail);
-router.delete('/:id', complaintController.deleteComplaint);
+router.post("/", validateUploadImage, prepareComplaintData, wrap(complaintController.create));
+router.get("/", wrap(complaintController.getAll));
+router.get("/:id", wrap(complaintController.getDetail));
+router.delete("/:id", wrap(complaintController.deleteComplaint));
 
 export default router;
