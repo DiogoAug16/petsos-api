be12b27caad744efb8be1153f848c09308afd8eb
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2026-03-25 19:53:09 -0400

    style: lint substituiu aspas simples para aspas duplas

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
-app.use('/api', routes);
+app.use("/uploads", express.static("uploads"));
+app.use("/api", routes);
 
 app.use(errorHandler);
 
