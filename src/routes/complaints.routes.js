71c96d2839cce2e3b9b590fc62e9dfb162b0a94d
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2026-05-06 16:03:53 -0400

    refactor: substituição de tratamento de dados manual para tratamento por schema em nearest

--- src/routes/complaints.routes.js
+++ src/routes/complaints.routes.js
@@ -2,19 +2,46 @@ import { Router } from "express";
 import * as complaintController from "../modules/complaints/complaints.controller.js";
 import { wrap } from "../shared/utils/async-handler.util.js";
 import { validateUploadImage } from "../validators/upload.validator.js";
-import { validateCreateComplaint, validateUpdateComplaint } from "../validators/complaint.validator.js";
+import { validateCreateComplaint, validateUpdateComplaint, validateNearestQuery } from "../validators/complaint.validator.js";
 import { authenticateToken } from "../shared/middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/", validateUploadImage, validateCreateComplaint, wrap(complaintController.create));
-router.get("/", wrap(complaintController.getAll));
-router.get("/nearest", wrap(complaintController.getNearest));
-router.get("/:id", wrap(complaintController.getDetail));
-router.patch("/:id", validateUploadImage, validateUpdateComplaint, wrap(complaintController.patchComplaint));
-router.delete("/:id", wrap(complaintController.deleteComplaint));
-router.post( "/:id/assumir", authenticateToken, wrap(complaintController.assumeComplaint)
+router.post(
+  "/",
+  authenticateToken,
+  validateUploadImage,
+  validateCreateComplaint,
+  wrap(complaintController.create)
 );
 
+router.get(
+  "/",
+  wrap(complaintController.getAll)
+);
+
+router.get(
+  "/nearest",
+  validateNearestQuery,
+  wrap(complaintController.getNearest)
+);
+
+router.get(
+  "/:id",
+  wrap(complaintController.getDetail)
+);
+
+router.patch(
+  "/:id",
+  validateUploadImage,
+  validateUpdateComplaint,
+  wrap(complaintController.patchComplaint)
+);
+
+router.delete(
+  "/:id",
+  authenticateToken,
+  wrap(complaintController.deleteComplaint)
+);
 
 export default router;
