4a31d80baaf5e251b69b68b78d021d23508f4cf8
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2026-05-10 21:34:12 -0400

    feat(users): adicionar perfil público por username

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,14 +1,32 @@
 import * as complaintService from "../complaints/complaints.service.js";
+import * as usersService from "./users.service.js";
 import { success } from "../../shared/utils/response.util.js";
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
-import { complaintResponseSchema } from "../../schemas/complaint.schema.js";
+import { publicComplaintSummarySchema } from "../../schemas/complaint.schema.js";
+import { publicUserProfileSchema } from "../../schemas/user.schema.js";
+
+/** @type {import("express").RequestHandler} */
+export const getMe = async (req, res) => {
+  const profile = await usersService.getPublicProfileById(req.userId);
+  const responseData = publicUserProfileSchema.parse(profile);
+  return success(res, responseData, StatusCodes.OK);
+};
+
+/** @type {import("express").RequestHandler} */
+export const getPublicProfile = async (req, res) => {
+  const profile = await usersService.getPublicProfileByUsername(
+    req.validatedParams.username,
+  );
+  const responseData = publicUserProfileSchema.parse(profile);
+  return success(res, responseData, StatusCodes.OK);
+};
 
 /** @type {import("express").RequestHandler} */
 export const getFollowedComplaints = async (req, res) => {
   const complaints = await complaintService.getFollowedByUsername(
     req.validatedParams.username,
   );
-  const responseData = z.array(complaintResponseSchema).parse(complaints);
+  const responseData = z.array(publicComplaintSummarySchema).parse(complaints);
   return success(res, responseData, StatusCodes.OK);
 };
