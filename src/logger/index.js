898fc0ec90dc3a3127ea07db7e33e3f15a19e429
DiogoAug16 <diogoaug16@outlook.com>
2026-06-05 17:51:31 -0400

    feat: implementa docker na api

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,46 +1,51 @@
 import pino from "pino";
 import { env } from "../config/env.js";
 
-const targets = [
+const baseTargets = [
   {
     target: "pino/file",
     level: "info",
     options: {
-      destination: "src/logger/logs/info.log",
+      destination: "./src/logger/logs/info.log",
+      mkdir: true,
     },
   },
   {
     target: "pino/file",
     level: "debug",
     options: {
-      destination: "src/logger/logs/debug.log",
+      destination: "./src/logger/logs/debug.log",
+      mkdir: true,
     },
   },
   {
     target: "pino/file",
     level: "error",
     options: {
-      destination: "src/logger/logs/error.log",
+      destination: "./src/logger/logs/error.log",
+      mkdir: true,
     },
   },
   {
     target: "pino/file",
     level: "fatal",
     options: {
-      destination: "src/logger/logs/fatal.log",
+      destination: "./src/logger/logs/fatal.log",
+      mkdir: true,
     },
   },
   {
     target: "pino/file",
     level: "warn",
     options: {
-      destination: "src/logger/logs/warn.log",
+      destination: "./src/logger/logs/warn.log",
+      mkdir: true,
     },
   },
 ];
 
 if (env.isDev) {
-  targets.unshift({
+  baseTargets.unshift({
     target: "pino-pretty",
     level: "debug",
     options: {
@@ -56,7 +61,7 @@ if (env.isDev) {
 const logger = pino({
   level: "debug",
   transport: {
-    targets,
+    targets: baseTargets,
   },
 });
 
